96793bd8cb811bd7b70db6fdd0f5429df2afbe9f
Pat-Mangubat23 <patrick.mangubat@randemgroup.com>
2024-08-16 22:13:39 +0800

    [ BugFix ] Fixed Logging

--- lib/wunderkind_method_channel.dart
+++ lib/wunderkind_method_channel.dart
@@ -19,6 +19,7 @@ class MethodChannelWunderkind extends WunderkindPlatform {
     required String webId,
     required bool isDebugMode,
   }) async {
+    debugPrint('[WDK Plugin] WebId: $webId, isDebugMode: $isDebugMode');
     await methodChannel.invokeMethod<void>('initialize', {
       "webId": webId,
       "isDebugMode": isDebugMode,
@@ -27,6 +28,7 @@ class MethodChannelWunderkind extends WunderkindPlatform {
 
   @override
   Future<void> setIsContextInfoTrackingEnabled(bool enabled) async {
+    debugPrint('[WDK Plugin] Info Tracking: $enabled');
     await methodChannel.invokeMethod<void>('setIsContextInfoTrackingEnabled', {
       "enabled": enabled,
     });
@@ -37,6 +39,7 @@ class MethodChannelWunderkind extends WunderkindPlatform {
     required String url,
     required ScreenType screen,
   }) async {
+    debugPrint('[WDK Plugin] WebId: url: $url, screen: ${screen.toString()}');
     await methodChannel.invokeMethod<void>('trackScreenView', {
       "url": url,
       "screen": screen.toScreenTypeString(),
@@ -45,6 +48,7 @@ class MethodChannelWunderkind extends WunderkindPlatform {
 
   @override
   Future<void> trackEmptyCart() async {
+    debugPrint('[WDK Plugin] Cart Emptied');
     await methodChannel.invokeMethod<void>('trackEmptyCart');
   }
 
@@ -53,6 +57,7 @@ class MethodChannelWunderkind extends WunderkindPlatform {
     required String itemId,
     required String groupId,
   }) async {
+    debugPrint('[WDK Plugin] itemId: $itemId, groupId: $groupId');
     await methodChannel.invokeMethod<void>('trackViewItem', {
       "itemId": itemId,
       "groupId": groupId,
@@ -64,6 +69,7 @@ class MethodChannelWunderkind extends WunderkindPlatform {
     required String groupId,
     required String feedId,
   }) async {
+    debugPrint('[WDK Plugin] groupId: $groupId, feedId: $feedId');
     await methodChannel.invokeMethod<void>('trackSelectSku', {
       "groupId": groupId,
       "feedId": feedId,
@@ -72,12 +78,14 @@ class MethodChannelWunderkind extends WunderkindPlatform {
 
   @override
   Future<void> trackAddToCart({required String itemId}) async {
+    debugPrint('[WDK Plugin] itemId: $itemId');
     await methodChannel
         .invokeMethod<void>('trackAddToCart', {"itemId": itemId});
   }
 
   @override
   Future<void> trackViewCategory({required WunderkindCategory category}) async {
+    debugPrint('[WDK Plugin] category: ${category.toMap()}');
     await methodChannel.invokeMethod<void>(
         'trackViewCategory', category.toMap());
   }
@@ -86,6 +94,7 @@ class MethodChannelWunderkind extends WunderkindPlatform {
   Future<void> trackViewSearch({
     required WunderkindCategory searchCategory,
   }) async {
+    debugPrint('[WDK Plugin] searchCategory: ${searchCategory.toMap()}');
     await methodChannel.invokeMethod<void>(
       'trackViewSearch',
       searchCategory.toMap(),
@@ -94,27 +103,34 @@ class MethodChannelWunderkind extends WunderkindPlatform {
 
   @override
   Future<void> trackLoggedIn(WunderkindCustomer customer) async {
+    debugPrint('[WDK Plugin] customer: ${customer.toMap()}');
     await methodChannel.invokeMethod<void>('trackLoggedIn', customer.toMap());
   }
 
   @override
   Future<void> trackLoggedOut() async {
+    debugPrint('[WDK Plugin] User logged out');
     await methodChannel.invokeMethod<void>('trackLoggedOut');
   }
 
   @override
   Future<void> trackTextOptIn({required String phone}) async {
+    debugPrint('[WDK Plugin] OptInPhoneNumber: phone');
     await methodChannel.invokeMethod<void>('trackTextOptIn', {'phone': phone});
   }
 
   @override
   Future<void> trackPurchase(WunderkindOrder order) async {
+    debugPrint('[WDK Plugin] order: ${order.toMap()}');
     await methodChannel.invokeMethod<void>('trackPurchase', order.toMap());
   }
 
   @override
   Future<void> setLogLevel(LogLevel level) async {
-    await methodChannel
-        .invokeMethod<void>('setLogLevel', {'level': level.toLogLevelString()});
+    debugPrint('[WDK Plugin] LogLevel: ${level.toString()}');
+    await methodChannel.invokeMethod<void>(
+      'setLogLevel',
+      {'level': level.toLogLevelString()},
+    );
   }
 }
